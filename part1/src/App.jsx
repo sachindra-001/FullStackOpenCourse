9e3c96885321da1afe2f349febd2231f4acc4375
sachindra-001 <gr5930404@gmail.com>
2025-12-25 22:01:15 +0530

    completed exercise1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,29 +3,35 @@ import Content from "./Content.jsx";
 import Total from "./Total.jsx";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const Part = [
-    {
-      part1: "Fundamentals of React",
-      exercises1: 10,
-    },
-    {
-      part2: "Using props to pass data",
-      exercises2: 7,
-    },
-    {
-      part3: "State of a component",
-      exercises3: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    Part: [
+      {
+        part1: "Fundamentals of React",
+        exercises1: 10,
+      },
+      {
+        part2: "Using props to pass data",
+        exercises2: 7,
+      },
+      {
+        part3: "State of a component",
+        exercises3: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content part1={Part[0]} part2={Part[1]} part3={Part[2]} />
+      <Header course={course.name} />
+      <Content
+        part1={course.Part[0]}
+        part2={course.Part[1]}
+        part3={course.Part[2]}
+      />
       <Total
-        exercises1={Part[0].exercises1}
-        exercises2={Part[1].exercises2}
-        exercises3={Part[2].exercises3}
+        exercises1={course.Part[0].exercises1}
+        exercises2={course.Part[1].exercises2}
+        exercises3={course.Part[2].exercises3}
       />
     </div>
   );
