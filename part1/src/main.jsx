b73f8e5096b0228809a742008e3b1932d4dd65c9
sachindra <your-email@example.com>
2026-01-18 11:10:04 +0530

    completed exercise 1.6

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,9 +1,22 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+// import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import App from "./App.jsx";
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+let counter = 1;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const refresh = () => {
+  root.render(<App counter={counter} />);
+};
+refresh();
+counter += 1;
+refresh();
+counter += 1;
+refresh();
