c944bff1bebe9b850b81c5ec1810afbc7c26c842
sachindra <your-email@example.com>
2026-01-18 11:28:58 +0530

    completed exercise 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad;
+
+  if (total === 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  const average = (props.good - props.bad) / total;
+  const positivePercentage = (props.good / total) * 100;
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercentage} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  // Calculate values for statistics
-  const total = good + neutral + bad;
-
-  // To avoid division by zero when the app starts, we can use a conditional
-  const average = total === 0 ? 0 : (good - bad) / total;
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100;
 
   return (
     <div>
@@ -19,15 +40,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-
-      {/* New statistics below */}
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercentage} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
