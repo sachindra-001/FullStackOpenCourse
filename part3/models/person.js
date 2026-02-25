a8336eeecfd34e0e8f63892b3b090aa403a6d536
sachindra <your-email@example.com>
2026-02-25 18:14:45 +0530

    done

---
+++ part3/models/person.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then((result) => console.log("connected to MongoDB"))
+  .catch((error) => console.log("error connecting to MongoDB:", error.message));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+// Transform the object to remove __v and change _id to a string id
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
