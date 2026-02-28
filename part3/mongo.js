a00059345799335f9523fd2efe385cabff577b48
sachindra-001 <gr5930404@gmail.com>
2026-02-28 11:26:20 +0530

    done

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,51 +1,51 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 // Check for minimum arguments
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
 // Replace <db_password> with the password variable and set your database name (e.g., phonebookApp)
-const url = `mongodb+srv://sachindravandse8_db_user:${password}@cluster0.cowdetd.mongodb.net/?appName=Cluster0`;
+const url = `mongodb+srv://sachindravandse8_db_user:${password}@cluster0.cowdetd.mongodb.net/?appName=Cluster0`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 // Define the Schema
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
 // Define the Model
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 // CASE 1: Only password provided - List all entries
 if (process.argv.length === 3) {
-  console.log("phonebook:");
+  console.log('phonebook:')
   Person.find({}).then((result) => {
     result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  });
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 }
 
 // CASE 2: Name and Number provided - Add new entry
 if (process.argv.length > 3) {
-  const name = process.argv[3];
-  const number = process.argv[4];
+  const name = process.argv[3]
+  const number = process.argv[4]
 
   const person = new Person({
     name: name,
     number: number,
-  });
+  })
 
   person.save().then(() => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
