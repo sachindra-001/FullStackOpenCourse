ec074910c13e9011d77b315f4c35df8f90c0b8b8
sachindra-001 <gr5930404@gmail.com>
2026-03-01 10:49:33 +0530

    completed exercise 4.2

---
+++ part4/BlogApp_backend/models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
