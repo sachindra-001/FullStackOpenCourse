ee9c3be2b6f5aca3e1b53e56320a73e1d18154fe
sachindra <your-email@example.com>
2026-03-14 21:22:07 +0530

    finaly completed til exercise 4.14

--- part4/BlogApp_backend/controller/blog.js
+++ part4/BlogApp_backend/controller/blog.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const { response } = require('express')
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async (request, response) => {
@@ -6,8 +7,12 @@ blogRouter.get('/', async (request, response) => {
   response.json(blog)
 })
 
-blogRouter.post('/', (request, response, next) => {
+blogRouter.post('/', async (request, response, next) => {
   const body = request.body
+
+  if (!body.title || !body.url) {
+    return response.status(400).json
+  }
 
   const blog = new Blog({
     title: body.title,
@@ -16,10 +21,42 @@ blogRouter.post('/', (request, response, next) => {
     likes: body.likes || 0,
   })
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.json(savedBlog)
+  try {
+    const savedBlog = await blog.save()
+    response.status(200).json(savedBlog)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogRouter.delete('/:id', async (request, response, next) => {
+  try {
+    // 2. Use the correct Model (Blog instead of Note)
+    await Blog.findByIdAndDelete(request.params.id)
+
+    // 3. 204 No Content is the correct status for a successful deletion
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+blogRouter.put('/:id', (request, response, next) => {
+  const { author, title, url, likes } = request.body
+
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (!blog) {
+        return response.status(404).end()
+      }
+
+      blog.author = author
+      blog.title = title
+      blog.url = url
+      blog.likes = likes
+
+      return blog.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
     })
     .catch((error) => next(error))
 })
--- part4/BlogApp_backend/test/blogapi.test.js
+++ part4/BlogApp_backend/test/blogapi.test.js
@@ -26,6 +26,105 @@ test('all notes are returned', async () => {
   console.log('Response Body:', response.body)
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
+test('blog posts have a unique identifier property named id', async () => {
+  const response = await api.get('/api/blog')
+
+  // ADD THIS LINE TO DEBUG:
+  console.log(
+    'Type of body:',
+    typeof response.body,
+    'Body content:',
+    response.body,
+  )
+
+  response.body.forEach((blog) => {
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
+})
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'fff.com',
+    likes: 15,
+  }
+
+  await api
+    .post('/api/blog')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogAtEnd = await helper.notesInDb()
+  assert.strictEqual(blogAtEnd.length, helper.initialBlogs.length + 1)
+
+  const titles = blogAtEnd.map((n) => n.title)
+  assert(titles.includes('First class tests'))
+})
+test('if likes property is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'Blog without likes',
+    author: 'Sachindra',
+    url: 'http://example.com',
+  }
+
+  const response = await api
+    .post('/api/blog')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+test.only('backend responds with 400 Bad Request if title or url are missing', async () => {
+  const newBlog = {
+    author: 'Robert C. Martin',
+    likes: 15,
+  }
+  await api.post('/api/blog').send(newBlog).expect(400)
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test.only('succeeds with status code 204', async () => {
+  const blogsAtStart = await helper.notesInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api.delete(`/api/blog/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.notesInDb()
+
+  const ids = blogsAtEnd.map((n) => n.id)
+  assert(!ids.includes(blogToDelete.id))
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+})
+
+test('succeeds with status code 200 if id is valid', async () => {
+  const blogsAtStart = await helper.notesInDb()
+  const blogToUpdate = blogsAtStart[0]
+
+  const updatedBlogData = {
+    title: blogToUpdate.title,
+    author: blogToUpdate.author,
+    url: blogToUpdate.url,
+    likes: blogToUpdate.likes + 1,
+  }
+
+  const response = await api
+    .put(`/api/blog/${blogToUpdate.id}`)
+    .send(updatedBlogData)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, blogToUpdate.likes + 1)
+  const blogsAtEnd = await helper.notesInDb()
+  const updatedBlogInDb = blogsAtEnd.find((b) => b.id === blogToUpdate.id)
+
+  assert.strictEqual(updatedBlogInDb.likes, blogToUpdate.likes + 1)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
